7d24a2b34cedde59fd501cb656b4a13f6efdc6a2
shun_a123 <sshuna123@gmail.com>
2022-09-13 16:08:55 +0900

    マップ＆スコアアイテムほぼ完成　佐藤

--- Assets/sato/Script/PopGenerater.cs
+++ Assets/sato/Script/PopGenerater.cs
@@ -6,16 +6,14 @@ public class PopGenerater : MonoBehaviour
 {
     public GameObject _enemyPrefab; // 敵プレハブ
     [SerializeField] Transform _popTransform; // 出現位置
-    private Rigidbody2D _rb;
     private float _interval; // リポップ秒数
-    [SerializeField] float _time = 0f; // 経過時間
+    float _time = 0f; // 経過時間
 
     bool isFirst = false;
 
 
     void Start()
     {
-        _rb = FindObjectOfType<Rigidbody2D>();
         _interval = 4f;
     }
 
@@ -26,11 +24,6 @@ public class PopGenerater : MonoBehaviour
 
         if (_time >= _interval)
         {
-
-            float vx = Random.Range(-5f, 5f);
-            float vy = Random.Range(-5f, 5f);
-            Vector3 force = new Vector3(vx, vy, 0);
-
             if (!isFirst)
             {
                 isFirst = true;
@@ -38,7 +31,6 @@ public class PopGenerater : MonoBehaviour
                 // インスタンスを生成してUnity側で出現位置を設定する
                 GameObject enemy = Instantiate(_enemyPrefab);
                 enemy.transform.position = new Vector3(_popTransform.position.x, _popTransform.position.y, 0);
-
             }
 
             _time = 0f; //時間を再計測
--- Assets/sato/Script/ScoreItem.cs
+++ Assets/sato/Script/ScoreItem.cs
@@ -6,6 +6,20 @@ public class ScoreItem : ItemBase2D
 {
     [SerializeField, Header("加算スコア")] int _addScore = 100;
 
+    private Rigidbody2D _rb;
+
+    private void Awake()
+    {
+        _rb = FindObjectOfType<Rigidbody2D>();
+    }
+    private void Start()
+    {
+        float vx = Random.Range(-5f, 5f);
+        float vy = Random.Range(-5f, 5f);
+        Vector3 force = new Vector3(vx, vy, 0);
+        _rb.AddForce(force, (ForceMode2D)ForceMode.Impulse);
+    }
+
     /// <summary>
     /// スコア取得時の加算
     /// </summary>
