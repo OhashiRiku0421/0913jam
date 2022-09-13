b83568437c6e79741153a7bafde06829abbf9440
RK <riku.kuu0421@gmail.com>
2022-09-13 11:16:29 +0900

    dotween入れたよ♡

---
+++ Assets/ohashi/GameManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int _score = 0;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
