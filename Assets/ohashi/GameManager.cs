5aaf1d5e99fabe5f2b07152000561c070718fb2b
RK <riku.kuu0421@gmail.com>
2022-09-13 11:54:07 +0900

    おｋ

---
+++ Assets/ohashi/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class GameManager : MonoBehaviour
+{
+    public static int _score = 0;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
